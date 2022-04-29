f6ac6752c2e1e7e840409cb09a46b4caef573980
ErickC <ErickCrowne@outlook.com>
2022-04-29 10:12:18 -0500

    Boolean while statement

---
+++ BooleanWhileStatement/BooleanWhileStatement/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BooleanWhileStatement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What planet in our Solar System has the fastest rotational speed?");
+            Console.WriteLine("Choices:\n'Mercury'\n'Venus'\n'Earth'\n'Mars'\n'Jupiter'\n'Saturn'\n'Uranus'\n'Neptune'\n'Pluto'");
+            string answer = Convert.ToString(Console.ReadLine());
+            bool isGuessed = false;
+
+            while (!isGuessed)
+            {
+                switch (answer)
+                {
+                    case "Mercury":
+                        Console.WriteLine("Mercury moves the fastest around the sun but it spins very slowly.");
+                        Console.WriteLine("Try again.");
+                        answer = Convert.ToString(Console.ReadLine());
+                        break;
+                    case "Venus":
+                        Console.WriteLine("Venus is the hottest, but not the fastest!");
+                        Console.WriteLine("Try again.");
+                        answer = Convert.ToString(Console.ReadLine());
+                        break;
+                    case "Earth":
+                        Console.WriteLine("It's not our planet!");
+                        Console.WriteLine("Try again.");
+                        answer = Convert.ToString(Console.ReadLine());
+                        break;
+                    case "Mars":
+                        Console.WriteLine("Mars' day is actually longer than Earths!");
+                        Console.WriteLine("Try again.");
+                        answer = Convert.ToString(Console.ReadLine());
+                        break;
+                    case "Jupiter":
+                        Console.WriteLine("Congratulations, you guessed it! A day on Jupiter is only 9 hours and 56 minutes!\nYet it's the biggest planet in the Solar System! You could fit 1300 Earths inside it.");
+                        isGuessed = true;
+                        break;
+                    case "Saturn":
+                        Console.WriteLine("You are so close... but this is still wrong");
+                        Console.WriteLine("Try again.");
+                        answer = Convert.ToString(Console.ReadLine());
+                        break;
+                    case "Uranus":
+                        Console.WriteLine("I know what you're thinking... wrong answer though.");
+                        Console.WriteLine("Try again.");
+                        answer = Convert.ToString(Console.ReadLine());
+                        break;
+                    case "Neptune":
+                        Console.WriteLine("Nope! Not Neptune! Keep guessing.");
+                        Console.WriteLine("Try again.");
+                        answer = Convert.ToString(Console.ReadLine());
+                        break;
+                    case "Pluto":
+                        Console.WriteLine("Pluto isn't even a planet, man! ");
+                        Console.WriteLine("Try again.");
+                        answer = Convert.ToString(Console.ReadLine());
+                        break;
+                    default:
+                        Console.WriteLine("Please spell the name exactly how it is quoted.");
+                        Console.WriteLine("Try again.");
+                        answer = Convert.ToString(Console.ReadLine());
+                        break;
+
+                }
+            }
+
+            Console.Read();
+        }
+    }
+}
--- BooleanWhileStatement/BooleanWhileStatement/Properties/AssemblyInfo.cs
+++ BooleanWhileStatement/BooleanWhileStatement/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Branching")]
+[assembly: AssemblyTitle("BooleanWhileStatement")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("HP Inc.")]
-[assembly: AssemblyProduct("Branching")]
+[assembly: AssemblyProduct("BooleanWhileStatement")]
 [assembly: AssemblyCopyright("Copyright © HP Inc. 2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("f9fa0929-739b-4a70-8f8c-a1c1f66b3967")]
+[assembly: Guid("5fc3abd1-1ef4-4c4e-8291-55d55f05464f")]
 
 // Version information for an assembly consists of the following four values:
 //
