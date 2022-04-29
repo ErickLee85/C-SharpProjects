c7a4b70e52a8f59031002a8ff2d9b772c918685b
ErickC <ErickCrowne@outlook.com>
2022-04-29 10:27:09 -0500

    added comments

--- BooleanWhileStatement/BooleanWhileStatement/Program.cs
+++ BooleanWhileStatement/BooleanWhileStatement/Program.cs
@@ -13,11 +13,11 @@ namespace BooleanWhileStatement
             Console.WriteLine("What planet in our Solar System has the fastest rotational speed?");
             Console.WriteLine("Choices:\n'Mercury'\n'Venus'\n'Earth'\n'Mars'\n'Jupiter'\n'Saturn'\n'Uranus'\n'Neptune'\n'Pluto'");
             string answer = Convert.ToString(Console.ReadLine());
-            bool isGuessed = false;
+            bool isGuessed = false; //setting boolean value to false so while loop will run
 
-            while (!isGuessed)
+            while (!isGuessed) //while boolean variable is false, do this!
             {
-                switch (answer)
+                switch (answer)//(switch) is basically a cleaner version of if/else statements. Passing in variable answer as argument to check each individual case against.
                 {
                     case "Mercury":
                         Console.WriteLine("Mercury moves the fastest around the sun but it spins very slowly.");
@@ -41,7 +41,7 @@ namespace BooleanWhileStatement
                         break;
                     case "Jupiter":
                         Console.WriteLine("Congratulations, you guessed it! A day on Jupiter is only 9 hours and 56 minutes!\nYet it's the biggest planet in the Solar System! You could fit 1300 Earths inside it.");
-                        isGuessed = true;
+                        isGuessed = true; //setting boolean value to true so switch loop will stop running. 
                         break;
                     case "Saturn":
                         Console.WriteLine("You are so close... but this is still wrong");
@@ -63,7 +63,7 @@ namespace BooleanWhileStatement
                         Console.WriteLine("Try again.");
                         answer = Convert.ToString(Console.ReadLine());
                         break;
-                    default:
+                    default: //If users input doesnt match any string variables from each case, execute this line of code...
                         Console.WriteLine("Please spell the name exactly how it is quoted.");
                         Console.WriteLine("Try again.");
                         answer = Convert.ToString(Console.ReadLine());
