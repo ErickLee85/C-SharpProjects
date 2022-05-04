d6d3bbd0bc5b8c1bf57cc0f5fcf786f341d25ab6
ErickC <ErickCrowne@outlook.com>
2022-05-04 15:07:12 -0500

    Class and Methods

---
+++ CallingMethods/CallingMethods/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallingMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int tryTimes = 0;
+            while (tryTimes < 3)//creating a short loop if user inputs wrong data type, the user will be able to retry automatically or just do more math operations if he/she chooses
+            {
+                try
+                {
+                    Console.WriteLine("Enter any number to do some math operations on using a Math class and 3 different Methods!");
+                    decimal theAnswer = Convert.ToDecimal(Console.ReadLine()); //casting users input into decimal! Always good to have the option for decimal just in case.
+
+                    decimal firstAnswer = mathClass.Multiply(theAnswer); //defining decimal variable by calling the Class "mathClass" and the method "Multiply" and passing in the argument "theAnswer" to my Method
+
+                    decimal secondAnswer = mathClass.Division(theAnswer);
+
+                    decimal thirdAnswer = mathClass.Subtraction(theAnswer);
+
+                    Console.WriteLine(theAnswer + " * 10 : " + firstAnswer +
+                                      "\n" + theAnswer + " / 10: " + secondAnswer +
+                                      "\n" + theAnswer + " - 10: " + thirdAnswer);
+                    
+
+                }
+
+                catch (FormatException ex) //using this catch for data format error, incase user types in string instead of decimal or integer
+                {
+                    Console.WriteLine(ex.Message);
+                }
+                finally
+                {
+                    tryTimes++;
+                }
+                
+            }Console.ReadLine();
+
+        }
+        
+    }
+    
+}
---
+++ CallingMethods/CallingMethods/mathClass.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallingMethods
+{
+    class mathClass //name of class
+    {
+        public static decimal Multiply(decimal methodOne) //parameter for this method, but will become argument with theAnswer variable when called 
+        {
+            return  methodOne * 10;
+        }
+       public static decimal Division(decimal methodTwo) 
+        {
+            return methodTwo / 10;
+        }
+
+        public static decimal Subtraction(decimal methodThree) 
+        {
+            return methodThree - 10;
+        }
+    }
+}
