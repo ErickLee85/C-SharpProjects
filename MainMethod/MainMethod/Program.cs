7b09305d3a45ff2de4457181336b0604d10cb687
ErickC <ErickCrowne@outlook.com>
2022-05-04 20:12:33 -0500

    MainMethod

---
+++ MainMethod/MainMethod/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            mathClass theObject = new mathClass(); //instantiating class by creating new object "theObject" in order to gain access to the methods
+
+            int firstResults = theObject.methodOne(100); //passing in integer as argument to methodOne
+            Console.WriteLine(firstResults);
+
+            int secondresults = theObject.methodOne(100.5m); //passing in decimal as argument to methodOne
+            Console.WriteLine(secondresults);
+
+            int thirdResults = theObject.methodOne("5"); //passing in string as argument to methodOne
+            Console.WriteLine(thirdResults);
+            Console.ReadLine();
+
+
+
+
+            
+
+        }
+    }
+}
---
+++ MainMethod/MainMethod/mathClass.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethod
+{
+    class mathClass
+    {
+        //I had to get rid of static, I could not access the methods with it active. 
+        //all methods are of data type int, regardless of incoming argument, due to returning data type is int, in this project.
+        public int methodOne(int answer1) //method with int parameter
+        {
+            return answer1 * 10; //returning answer1
+        }
+        public int methodOne(decimal answer2) //method with decimal paramter
+        {
+            int answer3 = Convert.ToInt32(answer2); //casting decimal to int 
+            return answer3 / 10; //returning answer3
+        }
+
+        public int methodOne(string answer3) //method with string parameter
+        {
+            int answer4 = Convert.ToInt32(answer3); //casting string to int
+            return answer4 + 10; //returning answer4
+        }
+    }
+}
