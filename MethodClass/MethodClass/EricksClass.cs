6924c3ae4a5cadd0f8aa680c6de412552475e15e
ErickC <ErickCrowne@outlook.com>
2022-05-05 15:38:01 -0500

    VoidMethod

---
+++ MethodClass/MethodClass/EricksClass.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VoidMethodClass 
+{
+    public class ericksClass 
+    {
+        public void methodOne(int param1, int param2) //using void to say my method will not return a value
+        {
+            int mathOp = param1 - 50;
+            Console.WriteLine(param1 + " - 50 = " + mathOp);
+            Console.WriteLine("Parameter 2 = " + param2 + "\n");
+            
+            
+        }
+    }
+}
+
---
+++ MethodClass/MethodClass/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VoidMethodClass
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            ericksClass newObject = new ericksClass(); //instantiating new object from Class ericksClass
+
+            newObject.methodOne(1000, 2000); //calling method by passing int arguments to methodOne of my class ericksClass
+
+            int argument1 = 50;
+            int argument2 = 100;
+
+            
+            newObject.methodOne(argument1, argument2); //calling method by passing in variables as arguments
+            Console.ReadLine();
+        }
+    }
+}
