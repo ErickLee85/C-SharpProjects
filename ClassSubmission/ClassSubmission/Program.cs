9c811f54d62820e2a825dd60a5501dcb02399657
ErickC <ErickCrowne@outlook.com>
2022-05-06 10:34:56 -0500

    Out param/Overloaded Method

---
+++ ClassSubmission/ClassSubmission/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassSubmission
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            ericksClass newObject = new ericksClass();   //instantiating new object from ericksClass
+
+            Console.WriteLine("Enter a number: ");
+            int usersNumber = Convert.ToInt32(Console.ReadLine());  //casting users input from string to integer!
+            newObject.methodOne(usersNumber);   //calling first methodOne with single variable
+
+            string b;    //initiliazing string variable but not giving it a value
+            newObject.outMethod(out b);  //calling output parameter from outMethod
+            Console.WriteLine($"The value of the out Parameter is {b}");
+
+            newObject.methodOne(450, 500);   //overloading same methodOne with 2 arguments
+
+
+
+
+
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ClassSubmission/ClassSubmission/ericksClass.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassSubmission
+{
+    public class ericksClass
+    {
+        public void methodOne(int param1)
+        {
+            int answer = param1 / 2;
+            Console.WriteLine($"methodOne says: {param1} / 2 = {answer}");
+        }
+        public void outMethod(out string a)
+        {
+            a = "Hello, World!";
+        }
+        public void methodOne(int param1, int param2)
+        {
+            Console.WriteLine($"This is methodOne again, overloaded with two parameters, {param1} and {param2}");
+        }
+
+
+    }
+    public static class wellAlrightyThen    //declaring static class
+    {
+
+    }
+}
