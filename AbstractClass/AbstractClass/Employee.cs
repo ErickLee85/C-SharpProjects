110e66f266ca68f3f10b89ebf1871451215c8bf6
ErickC <ErickCrowne@outlook.com>
2022-05-06 15:03:47 -0500

    Abstract Class

---
+++ AbstractClass/AbstractClass/Employee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClass
+{
+    class Employee : Person //Employee Class inheriting from abstract class Person
+    {
+        public override void SayName() //override keyword to access virtual method from Person class. 
+        {
+            base.SayName(); //basically this syntax is just calling the sayName method from Person Class.
+        }
+    }
+}
---
+++ AbstractClass/AbstractClass/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee newEmployee = new Employee(); //instantiating new object from Employee, you can not instantiate object from an Abstract class; eg: Person.
+            newEmployee.firstName = "Sample";//adding string to property of Employee class
+            newEmployee.lastName = "Student"; //adding string to property of Employee class
+            newEmployee.SayName(); //calling inherited method 
+            Console.ReadLine();
+        }
+    }
+}
