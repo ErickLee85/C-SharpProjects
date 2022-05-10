2470314fb59e6c2cd39122bdb220659b035431a7
ErickC <ErickCrowne@outlook.com>
2022-05-10 15:22:51 -0500

    Input

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your favorite prime number!");
+            int primeNumber = Convert.ToInt32(Console.ReadLine());
+            using (StreamWriter myFile = new StreamWriter(@"C:\Users\erick\OneDrive\Desktop\log1.txt", true))
+            {
+                myFile.WriteLine(primeNumber);
+                
+            }
+            string line;
+            using (StreamReader theFile = new StreamReader(@"C:\Users\erick\OneDrive\Desktop\log1.txt", true))
+            {
+                line = theFile.ReadLine();
+                while (line !=null)
+                {
+                    Console.WriteLine(line);
+                    line = theFile.ReadLine();                       
+                }
+            
+
+            }
+            Console.Read();
+
+
+
+        }
+    }
+}
---
+++ Input/Input/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Input
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your favorite prime number!");
+            int primeNumber = Convert.ToInt32(Console.ReadLine());
+            using (StreamWriter myFile = new StreamWriter(@"C:\Users\erick\OneDrive\Desktop\log1.txt", true))
+            {
+                myFile.WriteLine(primeNumber);
+
+            }
+            string line;
+            using (StreamReader theFile = new StreamReader(@"C:\Users\erick\OneDrive\Desktop\log1.txt", true))
+            {
+                line = theFile.ReadLine();
+                while (line != null)
+                {
+                    Console.WriteLine(line);
+                    line = theFile.ReadLine();
+                }
+
+
+            }
+            Console.Read();
+        }
+    }
+}
