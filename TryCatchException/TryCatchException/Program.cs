12664476c2477fb9e8f0729ab03603a7510654d7
ErickC <ErickCrowne@outlook.com>
2022-05-11 16:10:07 -0500

    try except

---
+++ TryCatchException/TryCatchException/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TryCatchException
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+           
+            Console.WriteLine("Please enter your age: \n");
+
+
+            try
+            {
+                int usersAge = Convert.ToInt32(Console.ReadLine());
+
+                if (usersAge <= 0)
+                {
+                    throw new ExceptionHandling();
+                }
+
+                var usersBirthday = DateTime.Today.AddYears(-usersAge);
+
+                Console.WriteLine(${usersBirthday}");
+                Console.Read();
+            }
+            catch (ExceptionHandling)
+            {
+                Console.WriteLine("Please enter a positive whole number.");
+                Console.ReadLine();
+               
+            
+            }
+
+            catch (Exception)
+            {
+                Console.WriteLine("An error occured.");
+                Console.ReadLine();
+                
+               
+            }
+
+       
+            
+
+            
+
+
+           
+
+
+
+            
+
+
+
+
+ 
+        }
+    }
+}
