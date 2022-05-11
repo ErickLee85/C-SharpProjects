c7c420e5591ed8511ab70b0d12fb4b2b4862d7ff
ErickC <ErickCrowne@outlook.com>
2022-05-11 13:53:45 -0500

    Constructor Assignment

---
+++ ConstructorAssignment/ConstructorAssignment/Person.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructorAssignment
+{
+    public class Person
+    {
+        public Person() : this("John", "Doe") //First constructor, which will be called if no parameters are used for new instantiated object. John Doe will be the default name. "this" is referring back to constructor below
+        {
+
+        }
+
+        public Person(string firstName, string lastName) //second constructor, which will be used if user passes in two string arguments to new object.
+        {
+            fName = firstName;
+            lName = lastName;
+        }
+
+        public string fName { get; set; } //first name property
+        public string lName { get; set; } //last name property
+
+
+    }
+}
---
+++ ConstructorAssignment/ConstructorAssignment/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructorAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int myPrimeNumber = 41; //integer constant, value can not be changed.
+
+            Console.WriteLine($"{myPrimeNumber}");
+
+            var myName = "Erick"; //using var here instead of starting syntax with "string myName"
+
+            Console.WriteLine($"{myName}");
+
+            Person obj1 = new Person(); //instantiating new Person object with no arguments.
+
+            Console.WriteLine($"{obj1.fName} {obj1.lName}");
+
+            Person obj2 = new Person("Erick", "Crowne"); //instantiating new Person object with two string arguments.
+            Console.WriteLine($"{obj2.fName} {obj2.lName}");
+            Console.ReadLine();
+
+        }
+
+
+    }
+}
+
+
+   
+
+    
