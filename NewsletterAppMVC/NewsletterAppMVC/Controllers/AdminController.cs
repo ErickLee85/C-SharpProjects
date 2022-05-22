ebc250d3e742093ce0b9ab37ed9274a68084ef59
Erick Crowne <erickcrowne85@gmail.com>
2022-05-22 08:05:37 -0500

    NewsLetterApp MVC

---
+++ NewsletterAppMVC/NewsletterAppMVC/Controllers/AdminController.cs
@@ -0,0 +1,48 @@
+﻿using NewsletterAppMVC.Models;
+using NewsletterAppMVC.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NewsletterAppMVC.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: Admin
+        public ActionResult Index()
+        {
+
+            using (NewsletterEntities db = new NewsletterEntities())
+            {
+                var signups = db.SignUps.Where(x => x.Removed == null).ToList();
+                //var signups = (from a in db.SignUps
+                //               where a.Removed == null
+                //               select a).ToList();        LINQ syntax 
+                var signupVms = new List<SignupVm>();
+                foreach (var signup in signups)
+                {
+                    var signupVM = new SignupVm();
+                    signupVM.Id = signup.Id;
+                    signupVM.FirstName = signup.FirstName;
+                    signupVM.LastName = signup.LastName;
+                    signupVM.EmailAddress = signup.EmailAddress;
+                    signupVms.Add(signupVM);
+                }
+                return View(signupVms);
+            }
+        }
+
+        public ActionResult Unsubscribe(int Id)
+        {
+            using (NewsletterEntities db = new NewsletterEntities())
+            {
+                var signup = db.SignUps.Find(Id);
+                signup.Removed = DateTime.Now;
+                db.SaveChanges();
+            }
+            return RedirectToAction("Index");
+        }
+    }
+}
