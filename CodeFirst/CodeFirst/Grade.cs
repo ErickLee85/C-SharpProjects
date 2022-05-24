4f1740a620a7540809199215f5271a2669ad47b3
Erick Crowne <erickcrowne85@gmail.com>
2022-05-24 11:10:56 -0500

    CodeFirst

---
+++ CodeFirst/CodeFirst/Grade.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeFirst
+{
+    public class Grade
+    {
+        public int GradeID { get; set; }
+        public string GradeName { get; set; }
+        public string Section { get; set; }
+
+        public ICollection<Student> Students { get; set; }
+    }
+}
---
+++ CodeFirst/CodeFirst/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace CodeFirst
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var ctx = new SchoolContext())
+            {
+                var stud = new Student() { StudentName = "Erick Crowne" };
+
+                ctx.Students.Add(stud);
+                ctx.SaveChanges();
+            }
+        }
+    }
+}
